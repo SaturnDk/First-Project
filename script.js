2dab5c59f44b1c24a9bc4c5be40878d08db01974
SaturnDk <Oliver.s.sartori@gmail.com>
2023-09-06 18:09:13 +0200

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const saturn = document.getElementById("SaturnSubmit")
+console.log(saturn)
+
+const val = document.getElementById('SaturnSubmit').value;
+console.log(val);
+
+// while(val !== "saturn"){
+//     console.log("ikke gættet")
+// } else {
+//     console.log("gættet!")
+// }
