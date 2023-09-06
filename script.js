d3da03ba713b3219607a10796a2751888479f804
SaturnDk <Oliver.s.sartori@gmail.com>
2023-09-06 18:40:32 +0200

    Opacity på Saturn og Input Field Background

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 
-const saturn = document.getElementById("SaturnSubmit")
-console.log(saturn)
 
-const val = document.getElementById('SaturnSubmit').value;
-console.log(val);
 
-// while(val !== "saturn"){
-//     console.log("ikke gættet")
-// } else {
-//     console.log("gættet!")
-// }
+// Assuming you have an input field with the id "planetInput" in your HTML
+const inputField = document.getElementById("SaturnSubmit");
+
+const saturnBillede = document.querySelector(".saturn") 
+
+
+// Add an event listener to the input field to detect changes
+inputField.addEventListener("input", function() {
+  const inputValue = inputField.value.toLowerCase(); // Convert input value to lowercase for case-insensitive comparison
+
+  if (inputValue === "saturn" || inputValue === "Saturn") {
+    console.log("Input value is 'saturn'");
+    inputField.style.backgroundColor="green";
+    saturnBillede.style.opacity ="1";
+    
+    
+} else if (inputValue === "") {
+inputField.style.backgroundColor="aqua";
+} else {
+    console.log("Input value is not 'saturn'");
+    
+    inputField.style.backgroundColor="red";
+    saturnBillede.style.opacity ="0";
+    
+
+
+  }
+});
